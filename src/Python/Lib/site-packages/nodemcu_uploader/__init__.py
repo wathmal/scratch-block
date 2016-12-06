f106f90bd9cc537773165e5777134b4249b20e38
dulaj-rajitha <dulaj.rajitha.12@cse.mrt.ac.lk>
2016-12-06 21:58:10 +0530

    remove python fom the app

--- src/Python/Lib/site-packages/nodemcu_uploader/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2015-2016 Peter Magnusson <peter@birchroad.net>
-
-"""Library and util for uploading files to NodeMCU version 0.9.4 and later"""
-
-from .version import __version__
-from .uploader import Uploader
